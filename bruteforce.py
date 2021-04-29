86ab26ed7799ade936f695b1a314eb1683fc1f85
devw4lL <dev_w4lL@protonmail.com>
2021-04-29 16:27:23 +0200

    ini

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,6 @@
 import pprint
+from collections import OrderedDict
+from operator import getitem
 import time
 import itertools
 
@@ -25,18 +27,17 @@ actions = [
     ["Action-19", 24, 21],
     ["Action-20", 114, 18],
 ]
-
 MAX_COST = 500
 
 
 def timeit(method):
-
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
         te = time.time()
-        print(f'Temps écoulé: {te-ts}')
+        print(f'Temps écoulé: {te - ts}')
         return result
+
     return timed
 
 
@@ -47,63 +48,46 @@ def add_gain(all_actions):
 def sort_by_return(all_actions):
     for i in range(len(all_actions)):
         for j in range(0, len(all_actions) - i - 1):
-            if all_actions[j][3] < all_actions[j+1][3]:
-                all_actions[j], all_actions[j+1] = all_actions[j+1], all_actions[j]
+            if all_actions[j][3] < all_actions[j + 1][3]:
+                all_actions[j], all_actions[j + 1] = all_actions[j + 1], all_actions[j]
+
+
+def sort_by_total_return(all_actions):
+    return OrderedDict(sorted(all_actions.items(), key=lambda x: getitem(x[1], 'gain'), reverse=True))
+
 
 @timeit
 def bruteforce(all_actions):
-    for permuts in itertools.permutations(range(len(all_actions)-1), r=None): #Permutation des indices
-        summ = 0
-        results = []
-        for index in permuts:
-            if summ < MAX_COST:
-                if (summ + all_actions[index+1][1]) < MAX_COST:
-                    summ += all_actions[index][1]
-                    results.append(all_actions[index])
-                else:
-                    cost = 0
-                    gain = 0
-                    for action in results:
-                        cost += action[1]
-                        gain += action[3]
-                    results.append(f'PRIX ACHAT DES ACTIONS: {cost}, GAIN TOTAL: {gain}')
-                    break
-        print(results)
-
-
-
-
-
+    all_results = {}
+    all_combinaison = []
+    count = 0
+    for i in range(len(all_actions)):
+        [all_combinaison.append(combis) for combis in
+         itertools.combinations(range(0, len(all_actions)), i)]  # Permutation des indices #Permutation des indices
+    all_combinaison.pop(0)
+    for combi in all_combinaison:
+        count += 1
+        all_results[count] = {'action': [], 'cout': 0, 'gain': 0}
+        for index in combi:
+            if all_results[count]['cout'] < MAX_COST and (all_results[count]['cout'] + all_actions[index][1]) < MAX_COST:
+                all_results[count]['action'].append(all_actions[index][0])
+                all_results[count]['cout'] += all_actions[index][1]
+                all_results[count]['gain'] += round(all_actions[index][3], 2)
+    return all_results
 
 
 def show_results(results):
-    rows = [['--', f'{result[0]})', f'{result[1]}', f'{result[2]}',
-             f'{result[3]}'] for result in results]
-    rows.insert(0, ["--", "NOM ACTION", "PRIX", "GAIN %", "GAIN EN EUROS"])
-    max_width = [max(map(len, col)) for col in zip(*rows)]
-    [print("  ".join((val.ljust(width) for val, width in zip(row, max_width)))) for row in rows]
+    rows = [['--', f'{result["action"]})', f'{result["cout"]}', f'{result["gain"]}'] for result in results.values()]
+    rows.insert(0, ["--", "NOM DES ACTION", "PRIX TOTAL", "GAIN APRES 2 ANS"])
+    max_width = [max(map(len, col)) for col in zip(*rows[:20])]
+    [print("  ".join((val.ljust(width) for val, width in zip(row, max_width)))) for row in rows[:20]]
     print("\n\r\n\r")
     return True
 
 
 if __name__ == '__main__':
-    # csv = open_csv('dataset1.csv')
-    # actions = add_gain(csv)
     add_gain(actions)
     sort_by_return(actions)
-    bruteforce(actions)
+    results = bruteforce(actions)
+    pprint.pprint(show_results(sort_by_total_return(results)))
 
-"""
-def bruteforce(all_actions):
-    summ = 0
-    gain = 0
-    results = []
-    while summ < MAX_COST:
-        for action in all_actions:
-            if action[1] < (MAX_COST - summ):
-                print('sum', summ, action, gain)
-                summ += action[1]
-                gain += action[3]
-                results.append(action)
-    return results
-"""
